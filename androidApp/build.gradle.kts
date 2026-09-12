c9074e6e4d8b7c431d5ad08c6980420bb7e24c9b
Terry Yiu <git@tyiu.xyz>
2026-09-12 12:02:59 +0300

    Bump build version to 13

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "xyz.tyiu.SatsPrice"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
+        versionCode = 13
         versionName = "2.0.0"
     }
     packaging {
